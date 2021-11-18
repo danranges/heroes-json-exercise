0896eca9eeb2a18385f68011473d71d93624794e
Dan Ranges <danranges@gmail.com>
2021-11-18 20:30:52 +0100

    Add XHR request for superhero JSON

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
+
+let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = () => {
+  const superHeroes = request.response;
+  populateHeader(superHeroes);
+  showHeroes(superHeroes);
+};
